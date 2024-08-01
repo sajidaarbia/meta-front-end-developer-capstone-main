97792855d79ba242b84e352f2f47dc3bc36424c5
sajidaarbia <sajidaarbia@gmail.com>
2024-08-01 16:13:52 +0200

    deploy react to github

---
+++ vit.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    base:"/meta-front-end-developer-capstone-main/"
+    Plugins: [react()],
+})
